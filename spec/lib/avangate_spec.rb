0d14db28d213b3f1ec4bebfd09cb9a1641f9509c
Leo <leo.majowka@gmail.com>
2015-10-13 15:12:18 +0300

    spec for SOAP#now

--- spec/lib/avangate_spec.rb
+++ spec/lib/avangate_spec.rb
@@ -4,8 +4,8 @@ module Avangate
 
   describe SOAP do
 
-    it 'should login' do
-      Avangate::SOAP.login
+    it 'should return now in the correct format' do
+      Avangate::SOAP.send(:now).should match /[0-9]+{4}-[0-9]+{2}-[0-9]+{2}\s[0-9]+{2}:[0-9]+{2}:[0-9]+{2}/
     end
 
   end
